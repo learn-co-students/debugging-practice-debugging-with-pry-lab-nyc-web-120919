bd7099551ef9560ae66d1d32e66328ab4f1ef2ca
Daniel Liu <danliu277@gmail.com>
2019-12-04 21:47:19 +0000

    Done.

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -4,7 +4,7 @@ class GasStation
   # It writes "get" for these two instance variables.
   # https://www.rubyguides.com/2018/11/attr_accessor/
   # https://stackoverflow.com/questions/20018784/attr-accessor-vs-attr-reader-instance-variables/20018831
-  attr_reader :brand, :unleaded_price
+  attr_accessor  :brand, :unleaded_price
 
   def initialize(brand, unleaded_price)
     @brand = brand
@@ -16,3 +16,7 @@ end
 petrol_petes = GasStation.new("Petrol Pete's", 50)
 seashore_shell = GasStation.new("The Seashore Shell", 40)
 dinobones = GasStation.new("Dino Bones Gas and Grill", 60)
+
+# let(:dinobones) {GasStation.new("Dino Bones Gas and Grill", 60)}
+#   it "is able to change the price of a gas station" do
+#     expect(dinobones.unleaded_price = 100).to eq(100)
